2ab8a7dc8b8b6195f7861ede585697427dcc5b5d
Javier Gamarra <nhpatt@gmail.com>
2013-02-10 20:06:09 +0100

    Android 1: Logging orientation change

--- src/com/nhpatt/mentha/activity/MainActivity.java
+++ src/com/nhpatt/mentha/activity/MainActivity.java
@@ -1,15 +1,19 @@
 package com.nhpatt.mentha.activity;
 
-import com.nhpatt.mentha.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.nhpatt.mentha.R;
 
 public class MainActivity extends Activity {
+
+	private static final String TAG = "MENTHA";
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		Log.e(TAG, "Starting MainActivity...");
 	}
 }

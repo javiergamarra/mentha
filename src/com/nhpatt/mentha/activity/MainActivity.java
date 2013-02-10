b7a846e8e6274a91b7ee2e47966a4fcaa076fed3
Javier Gamarra <nhpatt@gmail.com>
2013-02-10 23:41:07 +0100

    Android 6: OnClickListener y recuperar widgets

--- src/com/nhpatt/mentha/activity/MainActivity.java
+++ src/com/nhpatt/mentha/activity/MainActivity.java
@@ -3,10 +3,14 @@ package com.nhpatt.mentha.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.nhpatt.mentha.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	private static final String TAG = "MENTHA";
 
@@ -14,6 +18,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		Button save = (Button) findViewById(R.id.save);
+
+		save.setOnClickListener(this);
 		Log.e(TAG, "Starting MainActivity...");
+	}
+
+	@Override
+	public void onClick(final View v) {
+		EditText editText = (EditText) findViewById(R.id.transaction);
+		Log.e(TAG, editText.getText().toString());
 	}
 }

d10fbaafba299ddc16c07ebc35daa7dd2b9fbd95
Javier Gamarra <nhpatt@gmail.com>
2013-02-10 20:01:42 +0100

    Android 0 - New proyect!

---
+++ src/com/nhpatt/mentha/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nhpatt.mentha.activity;
+
+import com.nhpatt.mentha.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}

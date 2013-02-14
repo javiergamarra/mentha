4144bf6ef60f781c9142b1cabe48fa964b6f1815
Javier Gamarra <nhpatt@gmail.com>
2013-02-14 15:38:36 +0100

    Android 12: moving through activities

--- src/com/nhpatt/mentha/activity/HistoryAmountsActivity.java
+++ src/com/nhpatt/mentha/activity/HistoryAmountsActivity.java
@@ -1,15 +1,28 @@
 package com.nhpatt.mentha.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.nhpatt.mentha.R;
 
-public class HistoryAmountsActivity extends Activity {
+public class HistoryAmountsActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.history);
+
+		Button button = (Button) findViewById(R.id.history);
+		button.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(final View arg0) {
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
 	}
 }

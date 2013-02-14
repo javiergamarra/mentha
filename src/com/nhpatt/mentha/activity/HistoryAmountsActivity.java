f914fe3fe57129ce44ab47dc787148f52dbdb3c8
Javier Gamarra <nhpatt@gmail.com>
2013-02-14 15:30:42 +0100

    Android 10: new activity

---
+++ src/com/nhpatt/mentha/activity/HistoryAmountsActivity.java
@@ -0,0 +1,15 @@
+package com.nhpatt.mentha.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.nhpatt.mentha.R;
+
+public class HistoryAmountsActivity extends Activity {
+
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}

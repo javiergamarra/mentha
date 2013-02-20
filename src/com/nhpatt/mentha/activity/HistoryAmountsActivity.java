dac46355e7483b6a007715d3de1123512a66adeb
Javier Gamarra <nhpatt@gmail.com>
2013-02-21 00:52:36 +0100

    Android 23: contextual menus

--- src/com/nhpatt/mentha/activity/HistoryAmountsActivity.java
+++ src/com/nhpatt/mentha/activity/HistoryAmountsActivity.java
@@ -4,12 +4,19 @@ import static com.nhpatt.mentha.activity.Mentha.TRANSACTION;
 import android.app.ListActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
 import com.nhpatt.mentha.R;
 import com.nhpatt.mentha.adapter.AmountAdapter;
 import com.nhpatt.mentha.model.Transaction;
 
 public class HistoryAmountsActivity extends ListActivity {
+
+	private AmountAdapter adapter;
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
@@ -22,8 +29,28 @@ public class HistoryAmountsActivity extends ListActivity {
 		mentha.getTransactions().add(
 				(Transaction) getIntent().getSerializableExtra(TRANSACTION));
 
-		setListAdapter(new AmountAdapter(this,
-				android.R.layout.simple_list_item_1, mentha.getTransactions()));
+		adapter = new AmountAdapter(this, android.R.layout.simple_list_item_1,
+				mentha.getTransactions());
+		setListAdapter(adapter);
+
+		registerForContextMenu(getListView());
+	}
+
+	@Override
+	public void onCreateContextMenu(final ContextMenu menu, final View v,
+			final ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		menu.add("delete");
+	}
+
+	@Override
+	public boolean onContextItemSelected(final MenuItem item) {
+		final AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+				.getMenuInfo();
+		final Mentha mentha = (Mentha) getApplication();
+		mentha.getTransactions().remove(info.position);
+		adapter.notifyDataSetChanged();
+		return true;
 	}
 
 	private void recoverUser() {
